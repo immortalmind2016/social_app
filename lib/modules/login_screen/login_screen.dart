d2e87aa66c0353bc5ec5aaa49f896dc3b5b00b7f
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-14 21:09:21 +0200

    packegs updated

---
+++ lib/modules/login_screen/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Screen'),
+      ),
+      body: Container(
+        child: Text('Login Screen'),
+      ),
+    );
+  }
+}
