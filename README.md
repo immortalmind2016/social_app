6b416c0b997ff82f190969b2923777567d58f8d7
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-14 20:39:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# socialapp
+
+A new Flutter project.
+
---
+++ android/app/src/main/java/com/emansaeed/socialapp/socialapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.emansaeed.socialapp.socialapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
