be442f5144ed1d2f8fb4ceef9d912309699acfec
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-16 01:38:03 +0200

    cache helper completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,38 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:socialapp/BlocObserver.dart';
 import 'package:socialapp/modules/login_screen/login_screen.dart';
+import 'package:socialapp/modules/social_layout/social_layout.dart';
+import 'package:socialapp/shared/network/local/cache_helper.dart';
 import 'package:socialapp/shared/style/colors.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await CacheHelper.init();
 
   Bloc.observer = MyBlocObserver();
-  runApp(MyApp());
+  Widget widget;
+  var uid = CacheHelper.getData(key: 'uId');
+  if (uid != null){
+    widget = SocialLayout();
+  }
+  else{
+    widget = LoginScreen();
+  }
+  runApp(MyApp(
+      startWidget : widget,
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  final  Widget startWidget;
+  MyApp({this.startWidget});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: startWidget,
     theme:ThemeData(
     primarySwatch: defaultColor,
     appBarTheme: AppBarTheme(
